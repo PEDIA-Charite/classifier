80f6e1a045ffeb91e4a0e240f05ac0e36509d424
Tzung-Chien Hsieh <la60312@gmail.com>
2018-03-01 19:04:27 +0100

    Fix incorrect pedia JSON format
    Remove redundant gene_list
    Fix incorrect genomicData in pedia result JSON file.
    
    Fixed Bug #5
    Fixed Bug #6

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -22,8 +22,7 @@ if __name__ == '__main__':
         elif opt in ("--pedia"):
             pedia_path = arg
     results = []
-    defaultfeatures = ["combined_score",
-                       "feature_score", "gestalt_score", "has_mask"]
+    defaultfeatures = ["combined_score", "feature_score", "gestalt_score", "has_mask"]
     f_prefix = filename.split('/')[-1].split('.')[0]
     pedia_name = pedia_path
     pedia = {}
@@ -32,20 +31,12 @@ if __name__ == '__main__':
         for row in reader:
             pedia.update({row['gene_id']:row['pedia_score']})
     file_content = json.load(open(filename))
-    result = [] 
-    result.append(filename)
-    result.append(file_content['genomicData'])
-    results.append(result)
-    geneList = []
-    file_content["genomicData"] = result
     for gene_entry in file_content["geneList"]:
         gene_id = gene_entry['gene_symbol']
         score = pedia[gene_id]
         gene_entry.update({'pedia_score':score})
-        geneList.append(gene_entry)
 
     # adds the genelist to the json file
-    file_content["gene_list"] = geneList
     submitter = file_content['submitter']
     submitter_out = {'user_email':submitter['user_email'], 'user_name':submitter['user_name'], 'user_team':submitter['user_team']}
     file_content['submitter'] = submitter_out
